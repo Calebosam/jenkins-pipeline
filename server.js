ad59e78d9eb5735ef9deec3605b0a6d31d23db99
calebosam <caleb.osam@amalitech.com>
2023-06-21 23:48:07 +0000

    feat: update application

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require("express");
-const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
-console.log(PORT);
+console.log(`Application port is ${PORT}`);
